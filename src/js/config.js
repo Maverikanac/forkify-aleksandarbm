31eee4ab0ac50781f97df67ce0851bc8ee4c2df5
Maverikanac <aleksandar.b.mitic@gmail.com>
2023-04-04 20:02:55 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dc548d9d-4b74-4b3e-b231-6ef3561964bc';
+export const MODAL_CLOSE_SEC = 2.5;
